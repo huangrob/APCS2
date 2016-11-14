4637daa2571bbaf4b761891984ff75f2130db37b
huangrob <robert.huang2000@gmail.com>
2016-11-14 08:54:34 -0800

    FracCalc.2

--- Fraction Calculator/src/FracCalc.java
+++ Fraction Calculator/src/FracCalc.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class FracCalc {
 	
 	public static void main (String[] args){
@@ -6,15 +7,35 @@ public class FracCalc {
 		while (!userInput.equals("quit")){
 			System.out.println("Enter two mixed fractions (with underscore representing \"and\") separated by spaces and an operator.");
 			String mixedFraction = userInput.nextLine();
-			String[] produceAnswer = mixedFraction.split(" ");
-			System.out.println("Second Operand = " + produceAnswer[2]);
-			String[] produceAnswerWhole = Arrays.toString(produceAnswer).split("_");
-			System.out.println("Whole = " + produceAnswerWhole[0]);
+			System.out.println(produceAnswer(mixedFraction));
 			System.out.println("If you wish to quit, type quit");
 		}
-		
-		
+	}
+	
+	public static String produceAnswer(String mixedFraction){
+			
+		if (mixedFraction.contains(" ")){
+			String[] produceAnswer = mixedFraction.split(" ");
+			String secondOperand = produceAnswer[2];
+			return secondOperand;
+			String whole = "";
+			String fraction = "";
+			String numerator = "";
+			String denominator = "";
+			if (secondOperand.contains("_") && secondOperand.contains("/")){
+				String[] produceAnswerWhole = secondOperand.split("_");
+				whole = produceAnswerWhole[0];
+				fraction = produceAnswerWhole[1];
+				String[] produceAnswerFrac = fraction.split("/");
+				numerator = produceAnswerFrac[0];
+				denominator = produceAnswerFrac[1];
+
+			} else if (secondOperand.contains("/")){
+				whole = secondOperand;
+				numerator = "0";
+				denominator = "1";
+			}
 		
 	}
-
+	
 }
