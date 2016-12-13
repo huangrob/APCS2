b53c573db96cac683f592382f52e6d6c578888f7
huangrob <robert.huang2000@gmail.com>
2016-12-13 09:28:54 -0800

    ASCLLAND1

---
+++ ASCLLand/src/ASCLLand.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class ASCLLand {
+	public static void main(String[] args){
+		Scanner userInput = new Scanner(System.in);
+		for(int i = 0; i < 5; i++){
+			while (i < 5){
+				
+			}
+		}
+	}
+}
